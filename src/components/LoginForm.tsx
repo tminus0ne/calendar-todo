ebf9c057c1096f8828e1b63b7c8c3fdcf0d0621a
Greg-One <sgmdoc@yandex.ru>
2021-09-08 22:17:35 +0500

    add login states & inputs

--- src/components/LoginForm.tsx
+++ src/components/LoginForm.tsx
@@ -4,24 +4,34 @@ import { Button, Form, Input } from 'antd';
 
 import { rules } from '../utils/rules';
 import {AuthActionCreators} from "../store/reducers/auth/action-creators";
+import { useTypedSelector } from '../hooks/useTypedSelector';
 
 const LoginForm: React.FC = () => {
     const dispatch = useDispatch();
+    const {isLoading, error} = useTypedSelector(state => state.auth);
+    const [username, setUsername] = React.useState('');
+    const [password, setPassword] = React.useState('');
 
     const submit = () =>{
-        dispatch(AuthActionCreators.login('user', '123'));
+        dispatch(AuthActionCreators.login(username, password));
     }
 
     return (
         <Form
         onFinish={submit}
         >
+            {error && <div style={{ color: 'red' }}>
+                {error}
+            </div>}
             <Form.Item
                 label="Username"
                 name="username"
                 rules={[rules.required('Please input your username!')]}
             >
-                <Input />
+                <Input 
+                    value={username} 
+                    onChange={evt => setUsername(evt.target.value)}
+                />
             </Form.Item>
 
             <Form.Item
@@ -29,11 +39,15 @@ const LoginForm: React.FC = () => {
                 name="password"
                 rules={[rules.required('Please input your password!')]}
             >
-                <Input />
+                <Input
+                    type="password"
+                    value={password} 
+                    onChange={evt => setPassword(evt.target.value)}
+                />
             </Form.Item>
 
             <Form.Item wrapperCol={{ offset: 7, span: 16 }}>
-                <Button type="primary" htmlType="submit">
+                <Button type="primary" htmlType="submit" loading={isLoading}>
                     Login
                 </Button>
             </Form.Item>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Layout, Row } from 'antd';
+import { Card, Layout, Row } from 'antd';
 
 import LoginForm from '../components/LoginForm';
 
@@ -7,7 +7,9 @@ const Login: React.FC = () => {
     return (
         <Layout>
             <Row justify="center" align="middle" className="h100">
-                <LoginForm />
+                <Card>
+                    <LoginForm />
+                </Card>
             </Row>
         </Layout>
     );
