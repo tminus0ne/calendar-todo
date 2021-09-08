9ef29c2fc17d00526a83054db1c06a8e6974c449
Greg-One <sgmdoc@yandex.ru>
2021-09-08 22:49:46 +0500

    add setUser useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,20 @@ import { Layout } from 'antd';
 
 import AppRouter from './components/AppRouter';
 import Navbar from './components/Navbar';
+import { IUser } from './models/IUser';
+import { useActions } from './hooks/useActions';
 
 import './App.css';
 
 const App: React.FC = () => {
+    const { setUser, setIsAuth } = useActions()
+    React.useEffect(() => {
+        if (localStorage.getItem('auth')) {
+            setUser({ username: localStorage.getItem('username') || '' } as IUser);
+            setIsAuth(true);
+        }
+    }, []);
+
     return (
         <Layout>
           <Navbar />
