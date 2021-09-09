c933bd667b798a1c68607814aa0f6bbba5262113
Greg-One <sgmdoc@yandex.ru>
2021-09-09 17:18:46 +0500

    add createEvent action creator

--- src/store/reducers/event/action-creators.ts
+++ src/store/reducers/event/action-creators.ts
@@ -21,5 +21,18 @@ export const EventActionCreators = {
         } catch (error) {
             console.log(error);
         }
-    }
+    },
+
+    createEvent: (event: IEvent) => async (dispatch: AppDispatch) => {
+        try {
+            const events = localStorage.getItem('events') || '[]';
+            const json = JSON.parse(events) as IEvent[];
+            json.push(event);
+            dispatch(EventActionCreators.setEvents(json));
+
+            localStorage.setItem('events', JSON.stringify(json));
+        } catch (error) {
+            console.log(error);
+        }
+    } 
 }
